10b86725c97a454a95652440d22267ad6f808223
Vor <Vor@Vors-MacBook-Pro.local>
2013-11-19 00:59:16 -0800

    yyty

--- js-plugin/neko-contact-ajax-plugin/php/form-handler.php
+++ js-plugin/neko-contact-ajax-plugin/php/form-handler.php
@@ -4,8 +4,8 @@ if ( !$_POST ) exit;
 if ( !defined( "PHP_EOL" ) ) define( "PHP_EOL", "\r\n" );
 
 
-$to = "yourmail@yourdomain.com";
-$subject = "Website contact form ";
+$to = "design@designlaurels.com";
+$subject = "CustomPath Contact Form ";
 
 
 
@@ -16,25 +16,14 @@ foreach ($_POST as $key => $value) {
 }
 
 // Assign the input values to variables for easy reference
-$name      = @$_POST["name"];
+
 $email     = @$_POST["email"];
-$phone     = @$_POST["phone"];
-$message   = @$_POST["comment"];
-$verify    = @$_POST["verify"];
+
 
 
 // Test input values for errors
 $errors = array();
  //php verif name
-if(isset($_POST["name"])){
- 
-        if (!$name) {
-            $errors[] = "You must enter a name.";
-        } elseif(strlen($name) < 2)  {
-            $errors[] = "Name must be at least 2 characters.";
-        }
- 
-}
     //php verif email
 if(isset($_POST["email"])){
     if (!$email) {
@@ -43,36 +32,7 @@ if(isset($_POST["email"])){
         $errors[] = "You must enter a valid email.";
     }
 }
-    //php verif phone
-if(isset($_POST["phone"])){
-    if (!$phone) {
-        $errors[] = "You must enter a correct phone number.";
-    }elseif ( !is_numeric( $phone ) ) {
-        $errors[]= 'Your phone number can only contain digits.';
-    }
-}
-
-
-
-//php verif comment
-if(isset($_POST["comment"])){
-    if (strlen($message) < 10) {
-        if (!$message) {
-            $errors[] = "You must enter a message.";
-        } else {
-            $errors[] = "Message must be at least 10 characters.";
-        }
-    }
-}
-
-    //php verif captcha
-if(isset($_POST["verify"])){
-    if (!$verify) {
-        $errors[] = "You must enter the security code";
-    } else if (md5($verify) != $_SESSION['nekoCheck']['verify']) {
-        $errors[] = "The security code you entered is incorrect ";
-    }
-}
+ 
 
 if ($errors) {
         // Output errors and die with a failure message
@@ -95,12 +55,7 @@ if ($errors) {
     $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
     $mailBody  = "You have been contacted by $name" . PHP_EOL . PHP_EOL;
-    $mailBody .= (!empty($company))?'Company: '. PHP_EOL.$company. PHP_EOL . PHP_EOL:'';
-    $mailBody .= (!empty($quoteType))?'project Type: '. PHP_EOL.$quoteType. PHP_EOL . PHP_EOL:''; 
-    $mailBody .= "Message :" . PHP_EOL;
-    $mailBody .= $message . PHP_EOL . PHP_EOL;
     $mailBody .= "You can contact $name via email, $email.";
-    $mailBody .= (isset($phone) && !empty($phone))?" Or via phone $phone." . PHP_EOL . PHP_EOL:'';
     $mailBody .= "-------------------------------------------------------------------------------------------" . PHP_EOL;
 
 
